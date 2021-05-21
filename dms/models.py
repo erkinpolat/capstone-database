7e52f46010e9db48c53a898c8409e93e571805be
erkinpolat <44356468+erkinpolat@users.noreply.github.com>
2021-05-21 12:08:30 -0700

    faculty added to models.py

--- dms/models.py
+++ dms/models.py
@@ -19,8 +19,8 @@ class StudentJunior(models.Model):
 	customConc = models.CharField(max_length=100, blank=True)
 	minor = models.CharField(max_length=50, blank=True)
 	minorConc = models.CharField(max_length=50, blank=True)
-	knowledge2Offer = models.TextField()
-	knowledge2Request = models.TextField()
+	knowledge2Offer = models.TextField(blank=True)
+	knowledge2Request = models.TextField(blank=True)
 	section = models.CharField(max_length=50)
 	location = models.CharField(max_length=100)
 	topProjectIdea = models.TextField()
@@ -41,9 +41,33 @@ class StudentJunior(models.Model):
 		Upon saving a slug is created
 		'''
 		if not self.slug:
-			self.slug = slugify(self.name)
+			self.slug = slugify(self.email)
 		super().save(*args, **kwargs)
 
+class Faculty(models.Model):
+	email = models.EmailField()
+	name = models.CharField(max_length=200)
+	colleges = models.CharField(max_length=200)
+	coursesMinerva = models.TextField(blank=True)
+	coursesElsewhere = models.TextField(blank=True)
+	overviewExpertise = models.TextField(blank=True)
+	expertiseKeywords = models.TextField(blank=True)
+	additionalInterest = models.TextField(blank=True)
+	links = models.TextField(blank=True)
+	coursesDomainExpertise = models.TextField(blank=True)
+	slug = models.SlugField(max_length=200, blank=True)
+	created = models.DateField(auto_now_add=True, db_index=True)
+
+	def __str__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+		'''
+		Upon saving a slug is created
+		'''
+		if not self.slug:
+			self.slug = slugify(self.email)
+		super().save(*args, **kwargs)
 
 
 
