1b7f776cb595b4c6f4324a3af19545f39f4e808a
erkinpolat <44356468+erkinpolat@users.noreply.github.com>
2021-05-21 12:59:26 -0700

    added basic frontend

--- dms/urls.py
+++ dms/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'dms'
+
 urlpatterns = [
     path('', views.home, name="database-home"),
+    path('faculty/', views.faculty, name="database-faculty"),
 ]
--- dms/views.py
+++ dms/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('<h1> Welcome </h1>')
+	return render(request, 'dms/home.html')
+
+def faculty(request):
+	return render(request, 'dms/faculty.html')
