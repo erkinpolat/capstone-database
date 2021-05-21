3280395b7698f039aa404c4b649643f23b446b5b
erkinpolat <44356468+erkinpolat@users.noreply.github.com>
2021-05-21 12:17:28 -0700

    faculty register

--- dms/admin.py
+++ dms/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import StudentJunior
+from .models import StudentJunior, Faculty
 
 # Register your models here.
 
 @admin.register(StudentJunior)
+class StudentJuniorAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email']
+	list_filter = ['name']
+
+@admin.register(Faculty)
 class StudentJuniorAdmin(admin.ModelAdmin):
 	list_display = ['name', 'email']
 	list_filter = ['name']
